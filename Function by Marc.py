c95de5f63db8bc492c81a951e0d47c3251a3dc59
Marc Ganic <marc.o.ganic@gmail.com>
2023-03-15 15:28:40 +0100

    Work in progres

---
+++ Function by Marc.py
@@ -0,0 +1,14 @@
+def noSpace(message):
+    notab = message.replace("\t", "")
+    return notab.replace(" ", "")
+# Remeber to link noSpace to input
+def EnOrDe():
+    Ask = input("Type 0 to Encrypt or type 1 to Decrypt ")
+    while
+        if "0" in Ask:
+        print("Hello World")
+    elif "1" in Ask:
+        print("Hello World")
+    else:
+        input("Please try again")
+EnOrDe()
