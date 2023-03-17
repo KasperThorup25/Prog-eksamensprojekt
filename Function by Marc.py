1fdccc868575f4f8ba7acf54d8a2bada36a59d0d
Marc Ganic <marc.o.ganic@gmail.com>
2023-03-17 14:24:27 +0100

    Space and tab remover function, function to choose en or de cryption and function to add your message

--- Function by Marc.py
+++ Function by Marc.py
@@ -2,17 +2,26 @@ def noSpace(message):
     notab = message.replace("\t", "")
     return notab.replace(" ", "")
 # Remeber to link noSpace to input
-def EnOrDe():
-    X = True
-    while X is True:
-        Ask = input("Type 0 to Encrypt or type 1 to Decrypt: ")
-        if "0" in Ask:
+def EncryptOrDecrypt():
+    while True:
+        AskforEncryptOrDecrypt = input("Type 0 to Encrypt or type 1 to Decrypt: ")
+        if "0" in AskforEncryptOrDecrypt:
             print("\nYou Choose Encrypting.")
-            break
-        if "1" in Ask:
+            return 0
+        elif "1" in AskforEncryptOrDecrypt:
             print("\nYou Choose Decrypting.")
-            break
+            return 1
         else:
             print("\nERROR! Answer may only contain 0 or 1.")
             print("Please try again.\n")
             continue
+
+choice = EncryptOrDecrypt()
+def GetTextInput(choice):
+    if choice == 0:
+        Encrypt = input("\nPlease write the text you want to Encrypt: ")
+        print("\n",Encrypt)
+    elif choice == 1:
+        input("\nPlease write the text you want to Decrypt: ")
+
+GetTextInput(choice)
